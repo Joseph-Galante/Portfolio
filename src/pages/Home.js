e395c7f9a3750378e9c9f72e8e83c309945e7d09
Joe <jgalante00@gmail.com>
2021-06-09 11:18:26 -0400

    home page, general styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,10 +11,15 @@ const Home = () =>
     return (
         <div className="home-page">
             {redirect !== '' ? <Redirect to={redirect} /> : null}
-            <h1>Joe Galante</h1>
-            <h3>Electrical Engineer graduate turned Software Developer</h3>
-            <p>Focused on quality and robust design for web and mobile applications</p>
-            <input type="button" value="Read More About Me" onClick={() => {setRedirect('/about')}} />
+            <section className="home-pic">
+                <img src="https://i.imgur.com/d8OmLjW.jpg" alt="404"  />
+            </section>
+            <section className="home-details">
+                <h1 style={{fontSize: "36px"}}>Joe Galante</h1>
+                <h3>Electrical Engineer graduate turned Software Developer</h3>
+                <p>A curious tinkerer who works logically and efficiently, while maintaining a positive attitude. I enjoy taking on more challenging tasks in an effort to learn and sharpen my skills as quickly as possible. I take projects head on and with the confidence to exceed the project’s initial goals, with little to no extra guidance. I am passionate about working effectively with others while using standard practices to assure I am always providing clear and concise code</p>
+                <input type="button" value="Read More About Me" onClick={() => {setRedirect('/about')}} />
+            </section>
         </div>
     )
 }
