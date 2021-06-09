b0e9e1aca1d416c72c3c66feeca420a335c3b6b0
Joe <jgalante00@gmail.com>
2021-06-09 12:34:53 -0400

    home statement update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () =>
             <section className="home-details">
                 <h1 style={{fontSize: "36px"}}>Joe Galante</h1>
                 <h3>Electrical Engineer graduate turned Software Developer</h3>
-                <p>A curious tinkerer who works logically and efficiently, while maintaining a positive attitude. I enjoy taking on more challenging tasks in an effort to learn and sharpen my skills as quickly as possible. I take projects head on and with the confidence to exceed the project’s initial goals, with little to no extra guidance. I am passionate about working effectively with others while using standard practices to assure I am always providing clear and concise code</p>
+                <p>" A curious tinkerer who works logically and efficiently, while maintaining a positive attitude. I enjoy taking on more challenging tasks in an effort to learn and sharpen my skills as quickly as possible. I take projects head on and with the confidence to exceed the project’s initial goals, with little to no extra guidance. I am passionate about working effectively with others while using standard practices to assure I am always providing clear and concise code. "</p>
                 <input type="button" value="Read More About Me" onClick={() => {setRedirect('/about')}} />
             </section>
         </div>
