b0a5f5696c2868b53c8d811ae896a6d6f24d700d
Joe <jgalante00@gmail.com>
2021-06-11 12:35:59 -0400

    mobile styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,19 @@ const NavBar = () =>
         <nav>
             <div className="nav-left">
                 <Link id="home-link" to="/">
-                    <img src="https://i.imgur.com/WYms1v2.png" alt="404" width={300} />
+                    <img src="https://i.imgur.com/WYms1v2.png" alt="404" height="70%" />
                 </Link>
             </div>
             <div className="nav-right">
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/about">
+                    <h4>About</h4>
+                </Link>
+                <Link to="/projects">
+                    <h4>Projects</h4>
+                </Link>
+                <Link to="/contact">
+                    <h4>Contact</h4>
+                </Link>
             </div>
         </nav>
     )
